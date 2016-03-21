374a4dfba6c3b80651d05dc23a947299b4aeb31c
xieguigang <xie.guigang@gmail.com>
2016-03-21 15:46:34 +0800

    only works in the x86 compiling....

--- AI/SankeAI/LearnPlayGame.vb
+++ AI/SankeAI/LearnPlayGame.vb
@@ -1,3 +1,8 @@
-﻿Module LearnPlayGame
+﻿Imports Microsoft.VisualBasic.DataVisualization.DataMining.Framework
+
+Module LearnPlayGame
+
+    ReadOnly AI As New NeuralNetwork.Network(5, 6, 10)
+
 
 End Module
