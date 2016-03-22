2c6e007edf7a6ea16c40f20fae3a9334366b6dcf
xieguigang <xie.guigang@gmail.com>
2016-03-22 23:47:17 +0800

    failes on first try

--- AI/SankeAI/Program.vb
+++ AI/SankeAI/Program.vb
@@ -1,8 +1,21 @@
-﻿Module Program
+﻿Imports Microsoft.VisualBasic.Parallel
+
+
+Module Program
 
     Sub Main()
+
+        Dim game = New Snake.Form1
+        Call RunTask(AddressOf game.ShowDialog)
+        Call Threading.Thread.Sleep(2000)
+
+        Dim q As New QLAI(game.GameEngine)
+        game.GameEngine.ControlsMap.Enable = False
+
+        Call q.RunLearningLoop(200, Function(n) New QTable(n))
 
         Call LearnPlayGame.LearnPlay(5000)
         Pause()
     End Sub
+
 End Module
