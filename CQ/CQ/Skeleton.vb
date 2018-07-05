6335e5dfd73f200f32d764ce8925d2863dc40c28
xieguigang <xie.guigang@gcmodeller.org>
2018-07-05 21:28:42 +0800

    add character model

--- CQ/CQ/Skeleton.vb
+++ CQ/CQ/Skeleton.vb
@@ -1,6 +1,50 @@
-﻿''' <summary>
+﻿Imports System.Drawing
+
+''' <summary>
 ''' 
 ''' </summary>
 Public Class Skeleton
 
+    Public Property head As PointF
+    Public Property face As PointF
+    Public Property body As PointF
+    Public Property hair_left As PointF
+    Public Property hair_right As PointF
+    Public Property hair_back As PointF
+    Public Property left_hand As PointF
+    Public Property right_hand As PointF
+    Public Property foot_left As PointF
+    Public Property foot_right As PointF
+
 End Class
+
+''' <summary>
+''' 人物的图片材质数据结构
+''' </summary>
+Public Class Textures
+
+    Public Property Emotions As Dictionary(Of Emotions, Bitmap)
+    Public Property hair_left As Bitmap
+    Public Property hair_right As Bitmap
+    Public Property hair_back As Bitmap
+    Public Property head As Bitmap
+    Public Property body As Bitmap
+    Public Property left_hand As Bitmap
+    Public Property right_hand As Bitmap
+    Public Property foot As Bitmap
+
+End Class
+
+Public Enum Skeletons
+    head
+    face
+    body
+    hair_left
+    hair_right
+    ''' <summary>
+    ''' 两只脚好像是用同一个素材的
+    ''' </summary>
+    foot
+    left_hand
+    right_hand
+End Enum
--- CQ/CQ/SpiritImage.vb
+++ CQ/CQ/SpiritImage.vb
@@ -1,4 +1,50 @@
-﻿Public Class SpiritImage
+﻿Imports System.Drawing
 
+''' <summary>
+''' 精灵图模型，使用这个类来对人物的模型精灵图进行和切割操作
+''' </summary>
+Public Class SpiritImage
+
+    Public Property Emotions As Dictionary(Of Emotions, RectangleF)
+    Public Property hair_left As RectangleF
+    Public Property hair_right As RectangleF
+    Public Property hair_back As RectangleF
+    Public Property head As RectangleF
+    Public Property body As RectangleF
+    Public Property left_hand As RectangleF
+    Public Property right_hand As RectangleF
+    Public Property foot As RectangleF
+
+    Public Function Slice(texture As Bitmap) As Textures
+        SyncLock texture
+
+        End SyncLock
+    End Function
 End Class
 
+Public Enum Emotions As Integer
+    ''' <summary>
+    ''' 左上
+    ''' </summary>
+    terrified
+    ''' <summary>
+    ''' 右上
+    ''' </summary>
+    smile
+    ''' <summary>
+    ''' 左中
+    ''' </summary>
+    boring
+    ''' <summary>
+    ''' 右中
+    ''' </summary>
+    normal
+    ''' <summary>
+    ''' 左下
+    ''' </summary>
+    serious
+    ''' <summary>
+    ''' 右下
+    ''' </summary>
+    angry
+End Enum
