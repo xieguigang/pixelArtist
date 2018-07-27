427cc0c28911096b9c355b49ca9ad7c3a941b28c
xieguigang <xie.guigang@gcmodeller.org>
2018-07-27 20:24:48 +0800

    add wpf window

---
+++ CQ/SpiritSlicer/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Class MainWindow
+
+    Private Sub viewer_MouseWheel(sender As Object, e As MouseWheelEventArgs) Handles viewer.MouseWheel
+        Dim height# = viewer.Height
+        Dim width# = viewer.Width
+        Dim delta% = e.Delta
+
+        height += delta
+        width += delta
+
+        viewer.Height = If(height < 150, 150, height)
+        viewer.Width = If(width < 200, 200, width)
+    End Sub
+End Class
