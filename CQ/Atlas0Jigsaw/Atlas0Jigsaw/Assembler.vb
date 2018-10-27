aa34ec947d5cfb9c1a4f417dd54285c8d8f0dce5
xieguigang <xie.guigang@gcmodeller.org>
2018-10-27 17:24:46 +0800

    pixel delta

--- CQ/Atlas0Jigsaw/Atlas0Jigsaw/Assembler.vb
+++ CQ/Atlas0Jigsaw/Atlas0Jigsaw/Assembler.vb
@@ -12,4 +12,25 @@ Public Module Assembler
     Public Function Assembling(puzzle As IEnumerable(Of Image)) As Image
 
     End Function
+
+    ''' <summary>
+    ''' 将精灵图进行自动切割
+    ''' </summary>
+    ''' <param name="spirit"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Iterator Function Split(spirit As Image) As IEnumerable(Of Image)
+
+    End Function
+
+    Public Function Delta(p1 As Color, p2 As Color) As Double
+        Dim r# = p1.R - p2.R
+        Dim g# = p1.G - p2.G
+        Dim b# = p1.B - p2.B
+        Dim a# = p1.A - p2.A
+
+        Return {r, g, b, a} _
+            .Select(AddressOf Math.Abs) _
+            .Average
+    End Function
 End Module
