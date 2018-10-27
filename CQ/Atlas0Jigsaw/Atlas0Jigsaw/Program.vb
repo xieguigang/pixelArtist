ddf8baf6bfe755c3c814a357ced4305a589ab7db
xieguigang <xie.guigang@gcmodeller.org>
2018-10-27 16:41:35 +0800

    config for test

---
+++ CQ/Atlas0Jigsaw/Atlas0Jigsaw/Program.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Language.UnixBash
+
+Module Program
+
+    Sub Main()
+        Call test()
+    End Sub
+
+    Sub test()
+        Dim fragments As IEnumerable(Of Image) = (ls - l - r - "*.png" <= "E:\VB_GamePads\CQ\Atlas0Jigsaw\test").Select(AddressOf LoadImage)
+        Dim atlas As Image = fragments.Assembling
+
+        Call atlas.SaveAs("./test.png")
+    End Sub
+End Module
