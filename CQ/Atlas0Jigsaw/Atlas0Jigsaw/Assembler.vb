9f15343829f72a902aecd112658aa7769e551f1d
xieguigang <xie.guigang@gcmodeller.org>
2018-10-27 16:28:29 +0800

    add method

---
+++ CQ/Atlas0Jigsaw/Atlas0Jigsaw/Assembler.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+
+Public Module Assembler
+
+    ''' <summary>
+    ''' 将碎片拼接为一个完整的图片
+    ''' </summary>
+    ''' <param name="puzzle"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function Assembling(puzzle As IEnumerable(Of Image)) As Image
+
+    End Function
+End Module
