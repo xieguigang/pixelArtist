fb5188855745ac48af8e24221b7f6e2f535a780f
xieguigang <xie.guigang@gcmodeller.org>
2019-06-05 22:25:31 +0800

    get faces

--- CQ/Character/Slicer.vb
+++ CQ/Character/Slicer.vb
@@ -12,25 +12,36 @@ Public Module Slicer
         Return spirit.ImageCrop(New Point(width * 0.225, height * 0.38), New Size(width * 0.5, height * 0.4))
     End Function
 
-    Dim faceSize As New Size(50, 30)
+    Dim faceSize As New Size(40, 25)
 
     <Extension>
     Public Iterator Function Faces(spirit As Bitmap) As IEnumerable(Of Bitmap)
         ' A B
         ' C D
         ' E F
+        Dim x, x1, y As Integer
+
+        y = height * 0.01
+        x = width * 0.64
+        x1 = x + faceSize.Width + 3
 
         ' A
-        Yield spirit.ImageCrop(New Point(width * 0.6, height * 0.1), faceSize)
+        Yield spirit.ImageCrop(New Point(x, y), faceSize)
         ' B
-        Yield spirit.ImageCrop(New Point(width * 0.6 + faceSize.Width, height * 0.1), faceSize)
+        Yield spirit.ImageCrop(New Point(x1, y), faceSize)
+
+        y = height * 0.14
+
         ' C
-        Yield spirit.ImageCrop(New Point(width * 0.6, height * 0.2), faceSize)
+        Yield spirit.ImageCrop(New Point(x, y), faceSize)
         ' D
-        Yield spirit.ImageCrop(New Point(width * 0.6 + faceSize.Width, height * 0.2), faceSize)
+        Yield spirit.ImageCrop(New Point(x1, y), faceSize)
+
+        y = height * 0.26
+
         ' E
-        Yield spirit.ImageCrop(New Point(width * 0.6, height * 0.3), faceSize)
+        Yield spirit.ImageCrop(New Point(x, y), faceSize)
         ' F
-        Yield spirit.ImageCrop(New Point(width * 0.6 + faceSize.Width, height * 0.3), faceSize)
+        Yield spirit.ImageCrop(New Point(x1, y), faceSize)
     End Function
 End Module
