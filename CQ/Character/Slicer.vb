99f7bd30663b848f4f8a8b6cdcff02c67b9a7155
xieguigang <xie.guigang@gcmodeller.org>
2019-06-05 22:17:41 +0800

    try to cut faces

--- CQ/Character/Slicer.vb
+++ CQ/Character/Slicer.vb
@@ -9,6 +9,28 @@ Public Module Slicer
 
     <Extension>
     Public Function Head(spirit As Bitmap) As Bitmap
-        Return spirit.ImageCrop(New Point(width * 0.3, height * 0.3), New Size(width * 0.1, height * 0.1))
+        Return spirit.ImageCrop(New Point(width * 0.225, height * 0.38), New Size(width * 0.5, height * 0.4))
+    End Function
+
+    Dim faceSize As New Size(50, 30)
+
+    <Extension>
+    Public Iterator Function Faces(spirit As Bitmap) As IEnumerable(Of Bitmap)
+        ' A B
+        ' C D
+        ' E F
+
+        ' A
+        Yield spirit.ImageCrop(New Point(width * 0.6, height * 0.1), faceSize)
+        ' B
+        Yield spirit.ImageCrop(New Point(width * 0.6 + faceSize.Width, height * 0.1), faceSize)
+        ' C
+        Yield spirit.ImageCrop(New Point(width * 0.6, height * 0.2), faceSize)
+        ' D
+        Yield spirit.ImageCrop(New Point(width * 0.6 + faceSize.Width, height * 0.2), faceSize)
+        ' E
+        Yield spirit.ImageCrop(New Point(width * 0.6, height * 0.3), faceSize)
+        ' F
+        Yield spirit.ImageCrop(New Point(width * 0.6 + faceSize.Width, height * 0.3), faceSize)
     End Function
 End Module
--- CQ/test/characterVisualtest.vb
+++ CQ/test/characterVisualtest.vb
@@ -1,16 +1,21 @@
 ﻿Imports System.Drawing
 Imports Character
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Linq
 
 Module characterVisualtest
     Sub Main()
         Call sliceImages("E:\VB_GamePads\CQ\pr_6_1\texture_cos_wi_17_20.png", "E:\VB_GamePads\CQ\pr_6_1\slices")
-
+        Call sliceImages("E:\VB_GamePads\CQ\pr_6_1\texture_pr_6_1.png", "E:\VB_GamePads\CQ\pr_6_1\slices")
     End Sub
 
     Sub sliceImages(source As String, output$)
         Dim texture As Bitmap = source.LoadImage
 
         Call texture.Head.SaveAs($"{output}/{source.BaseName}/head.png")
+
+        For Each face As SeqValue(Of Bitmap) In texture.Faces.SeqIterator
+            Call face.value.SaveAs($"{output}/{source.BaseName}/face_{face.i}.png")
+        Next
     End Sub
 End Module
