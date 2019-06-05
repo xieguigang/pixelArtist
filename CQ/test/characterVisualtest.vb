c151d843de9b9a5058de2916fae4c80be9a19feb
xieguigang <xie.guigang@gcmodeller.org>
2019-06-05 22:06:08 +0800

    config test

--- CQ/test/characterVisualtest.vb
+++ CQ/test/characterVisualtest.vb
@@ -4,9 +4,13 @@ Imports Microsoft.VisualBasic.Imaging
 
 Module characterVisualtest
     Sub Main()
-        Dim texture As Bitmap = "E:\VB_GamePads\CQ\pr_6_1\texture_cos_wi_17_20.png".LoadImage
+        Call sliceImages("E:\VB_GamePads\CQ\pr_6_1\texture_cos_wi_17_20.png", "E:\VB_GamePads\CQ\pr_6_1\slices")
 
-        Call texture.Head.SaveAs("./head.png")
+    End Sub
 
+    Sub sliceImages(source As String, output$)
+        Dim texture As Bitmap = source.LoadImage
+
+        Call texture.Head.SaveAs($"{output}/{source.BaseName}/head.png")
     End Sub
 End Module
