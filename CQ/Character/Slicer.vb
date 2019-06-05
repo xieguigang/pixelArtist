4220598dda166d55935033bedc50cb9bda0c6aa3
xieguigang <xie.guigang@gcmodeller.org>
2019-06-05 22:03:54 +0800

    add slicer test

---
+++ CQ/Character/Slicer.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging.BitmapImage
+
+Public Module Slicer
+
+    Const width = 256
+    Const height = 256
+
+    <Extension>
+    Public Function Head(spirit As Bitmap) As Bitmap
+        Return spirit.ImageCrop(New Point(width * 0.3, height * 0.3), New Size(width * 0.1, height * 0.1))
+    End Function
+End Module
---
+++ CQ/test/characterVisualtest.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Drawing
+Imports Character
+Imports Microsoft.VisualBasic.Imaging
+
+Module characterVisualtest
+    Sub Main()
+        Dim texture As Bitmap = "E:\VB_GamePads\CQ\pr_6_1\texture_cos_wi_17_20.png".LoadImage
+
+        Call texture.Head.SaveAs("./head.png")
+
+    End Sub
+End Module
