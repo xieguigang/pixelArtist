c326db9adb6ebd8ff144194a324b34890c4e25a5
xieguigang <xie.guigang@gcmodeller.org>
2019-06-09 16:10:29 +0800

    add image resource model

---
+++ CQ/Character/Assembler.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+
+Public Module Assembler
+
+    <Extension>
+    Public Function Draw(visual As Character) As Bitmap
+
+    End Function
+End Module
