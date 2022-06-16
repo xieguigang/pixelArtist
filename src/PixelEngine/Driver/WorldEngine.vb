ec2d08b4d71bdeceb953abc44b90cf6abc5f7b51
xieguigang <xie.guigang@gcmodeller.org>
2022-06-16 21:27:29 +0800

    update screen?

---
+++ src/PixelEngine/Driver/WorldEngine.vb
@@ -0,0 +1,65 @@
+﻿Imports Microsoft.VisualBasic.Parallel.Tasks
+
+Public Class WorldEngine : Implements IDisposable
+
+    Dim graphics As Action(Of PixelGraphics)
+    Dim graphicsLoop As UpdateThread
+    Dim gameLoop As UpdateThread
+    Dim screen As PixelScreen
+
+    Private disposedValue As Boolean
+
+    Sub New(graphics As Action(Of PixelGraphics), controls As Action, fps As Integer)
+        Dim ms As Double = 1000 / fps
+
+        Me.graphics = graphics
+        Me.graphicsLoop = New UpdateThread(ms, AddressOf PaintFrame)
+        Me.gameLoop = New UpdateThread(ms, controls)
+    End Sub
+
+    Public Function LoadScreenDevice(screen As PixelScreen) As WorldEngine
+        Me.screen = screen
+        Return Me
+    End Function
+
+    Public Sub PaintFrame()
+        Call graphics(screen.RequestFrame)
+    End Sub
+
+    Public Sub Run()
+        Call graphicsLoop.Start()
+        Call gameLoop.Start()
+    End Sub
+
+    Public Sub Pause()
+        Call graphicsLoop.Stop()
+        Call gameLoop.Stop()
+    End Sub
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects)
+                Call gameLoop.Dispose()
+                Call graphicsLoop.Dispose()
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            ' TODO: set large fields to null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: override finalizer only if 'Dispose(disposing As Boolean)' has code to free unmanaged resources
+    ' Protected Overrides Sub Finalize()
+    '     ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
+End Class
--- src/PixelEngine/PixelScreen.vb
+++ src/PixelEngine/PixelScreen.vb
@@ -1,3 +1,16 @@
-﻿Public Class PixelScreen
+﻿Public Class PixelScreen : Implements IDisposable
+
+    Public Property Resolution As Size
+
+    Public Function RequestFrame() As PixelGraphics
+        Dim g As Graphics = Me.CreateGraphics
+        Dim frame As PixelGraphics = PixelGraphics.FromGdiDevice(g, Size)
+
+        Call g.Clear(BackColor)
+        Call frame.SetScreenResolution(Resolution.Width, Resolution.Height)
+        Call g.Flush()
+
+        Return frame
+    End Function
 
 End Class
