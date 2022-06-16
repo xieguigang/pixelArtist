f3e822b5c18e6ff4b13b6d04307246765ddbe604
xieguigang <xie.guigang@gcmodeller.org>
2022-06-16 21:35:57 +0800

    renames

--- src/SnakeEvolution/FormGame.Designer.vb
+++ src/SnakeEvolution/FormGame.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class FormGame
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
---
+++ src/SnakeEvolution/FormGame.vb
@@ -0,0 +1,3 @@
+﻿Public Class FormGame
+
+End Class
