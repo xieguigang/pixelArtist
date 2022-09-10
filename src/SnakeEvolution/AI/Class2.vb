3ef85917fa0ca830e1714caac108b41c52b7e611
xieguigang <xie.guigang@gcmodeller.org>
2022-09-10 12:19:57 +0800

    imports old source code

---
+++ src/SnakeEvolution/AI/Class2.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic.DataMining.QLearning
+Imports Microsoft.VisualBasic.DataMining.QLearning.DataModel
+Imports Microsoft.VisualBasic.MachineLearning.QLearning.DataModel
+
+''' <summary>
+''' 方向加下一个动作作为key
+''' </summary>
+Public Class QTable : Inherits QTable(Of GameControl)
+
+    Sub New(range As Integer)
+        Call MyBase.New(range)
+    End Sub
+
+    Sub New(model As QModel)
+        Call MyBase.New(model)
+    End Sub
+
+    Protected Overrides Function __getMapString(map As GameControl) As String
+        Return map.ToString
+    End Function
+End Class
