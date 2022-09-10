577cd273a35aff0848751b8a2abacb4d5dadb60a
xieguigang <xie.guigang@gcmodeller.org>
2022-09-10 12:29:13 +0800

    add geometric helper

---
+++ src/PixelEngine/Controller/Controller.vb
@@ -0,0 +1,56 @@
+﻿''' <summary>
+''' Capture the input device actions, including mouse input and keyboard key pressing.
+''' (捕捉动作的输入的设备，包括键盘和鼠标)
+''' </summary>
+Public Class Controller : Inherits Abstract.EngineParts
+
+    Dim WithEvents _innerDevice As PixelScreen
+    Dim _actionCallback As Action(Of Controls, Char)
+    Dim _clickObject As Action(Of Point, GraphicUnit)
+
+    ''' <summary>
+    ''' The gamepad input mappings, mapping the keyboard input as the internal actions.
+    ''' (输入映射，将键盘按键映射为游戏内部的动作信号)
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property ControlMaps As ControlMaps = New ControlMaps
+    Public Property Enable As Boolean = True
+
+    Sub New(engine As GameEngine)
+        Call MyBase.New(engine)
+
+        _innerDevice = engine.display
+        _actionCallback = AddressOf engine.Invoke
+        _clickObject = AddressOf engine.ClickObject
+    End Sub
+
+    Private Sub _innerDevice_KeyPress(sender As Object, e As KeyPressEventArgs) Handles _innerDevice.KeyPress
+        If Not Enable Then
+            Return
+        End If
+
+        Dim key As Char = e.KeyChar
+        Dim action = ControlMaps.GetMapAction(key)
+        Call _actionCallback.BeginInvoke(action, key, Nothing, Nothing)
+    End Sub
+
+    Private Sub _innerDevice_MouseClick(sender As Object, e As MouseEventArgs) Handles _innerDevice.MouseClick
+        If Not Enable Then
+            Return
+        End If
+
+        Dim xy As Point = e.Location
+
+        Call Engine _
+            .Select(Function(x) __invokeClick(x, xy)) _
+            .ToArray
+    End Sub
+
+    Private Function __invokeClick(x As GraphicUnit, pos As Point) As Boolean
+        If x.Region.Contains(pos) Then
+            Call _clickObject.BeginInvoke(pos, x, Nothing, Nothing)
+        End If
+
+        Return True
+    End Function
+End Class
---
+++ src/PixelEngine/Geometric.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module Geometric
+
+    ''' <summary>
+    ''' <paramref name="b"/> position relatives to <paramref name="a"/>
+    ''' </summary>
+    ''' <param name="a"></param>
+    ''' <param name="b"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function Position(a As Point, b As Point, Optional max As Boolean = False) As Controls
+        Dim v, h As Controls
+        Dim dv, dh As Integer
+
+        If b.X > a.X Then
+            h = Controls.Right
+        ElseIf b.X < a.X Then
+            h = Controls.Left
+        End If
+
+        dh = Math.Abs(a.X - b.X)
+
+        If b.Y > a.Y Then
+            v = Controls.Down
+        ElseIf b.Y < a.Y Then
+            v = Controls.Up
+        End If
+
+        dv = Math.Abs(a.Y - b.Y)
+
+        If max Then
+            If dh > dv Then
+                Return h
+            Else
+                Return v
+            End If
+        Else
+            Return h Or v
+        End If
+    End Function
+
+    Public Function PointTo(a As Point, b As Point, d As Controls, Optional max As Boolean = False) As Boolean
+        Dim pos As Controls = Position(a, b, max)
+
+        If pos.HasFlag(Controls.Down) <> d.HasFlag(Controls.Down) Then
+            Return False
+        End If
+        If pos.HasFlag(Controls.Left) <> d.HasFlag(Controls.Left) Then
+            Return False
+        End If
+        If pos.HasFlag(Controls.Right) <> d.HasFlag(Controls.Right) Then
+            Return False
+        End If
+        If pos.HasFlag(Controls.Up) <> d.HasFlag(Controls.Up) Then
+            Return False
+        End If
+
+        Return True
+    End Function
+End Module
