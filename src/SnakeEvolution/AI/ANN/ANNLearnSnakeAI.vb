d342d1a8038a1dbbeb8ec01e126241aad477c674
xieguigang <xie.guigang@gcmodeller.org>
2022-09-12 10:26:13 +0800

    play by ANN AI

--- src/SnakeEvolution/AI/ANN/ANNLearnSnakeAI.vb
+++ src/SnakeEvolution/AI/ANN/ANNLearnSnakeAI.vb
@@ -1,20 +1,99 @@
-﻿Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork
-Imports Microsoft.VisualBasic.MachineLearning.QLearning
+﻿Imports Microsoft.VisualBasic.Imaging.Math2D
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork
+Imports PixelArtist.Engine
+Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
 
 Public Class ANNLearnSnakeAI
 
     Dim core As Network
+    Dim game As SnakeGameEngine
+    Dim stat As QState
 
-    Sub New()
-        core = New Network(
-            inputSize:=inputSize,
-            hiddenSize:=hiddenSize,
-            outputSize:=outputSize,
-            learnRate:=learnRate,
-            momentum:=momentum,
-            active:=active,
-            weightInit:=weightInit
+    Sub New(game As SnakeGameEngine)
+        Me.game = game
+        Me.core = New Network(
+            inputSize:=20,
+            hiddenSize:={8, 6, 5},
+            outputSize:=4,
+            learnRate:=0.1,
+            momentum:=0.9
         )
+        Me.stat = New QState(game)
     End Sub
 
+    Public Sub Run()
+        For i As Integer = 0 To Integer.MaxValue
+            Call Run(i)
+        Next
+    End Sub
+
+    Private Sub Run(i As Integer)
+        ' Get environment state as input
+        Dim pre = game.snake.Head.Distance(game.food.Location)
+
+        Call stat.SetState(stat.GetNextState(Nothing))
+
+        Dim v As Double() = stat.Current.ToVector
+        Dim output = core.ForwardPropagate(v, parallel:=False)
+        Dim controls = output.Select(Function(n) If(n.Value < 0.5, 0, n.Value)).ToArray
+        Dim index = which.Max(controls)
+        Dim action As Controls = PixelArtist.Engine.Controls.NotBind
+
+        If controls.Any(Function(c) c > 0) Then
+            ' Gamepad action decodes
+            Select Case index
+                Case 0
+                    action = PixelArtist.Engine.Controls.Up
+                Case 1
+                    action = PixelArtist.Engine.Controls.Down
+                Case 2
+                    action = PixelArtist.Engine.Controls.Left
+                Case 3
+                    action = PixelArtist.Engine.Controls.Right
+                Case Else
+                    action = PixelArtist.Engine.Controls.NotBind
+            End Select
+        End If
+
+        ' QL_AI press the button on the gamepad
+        Call game.Invoke(action)
+        ' 20220910
+        '
+        ' wait for the action to take effects
+        '
+        ' the sleep time should be nearby the 
+        ' word reactor its update time
+        ' or the AI can not be learn due to the
+        ' reason of world is already been updated
+        ' too many loops when we test the condition
+        ' below
+        Call Threading.Thread.Sleep(game.game.worldSpeed + 10)
+
+        ' Calculate the distance between the snake head and the target food
+        Dim now = game.snake.Head.Distance(game.food.Location)
+
+        If now < pre Then
+            ' If the distance result of the new action compare with the previous
+            ' action is getting smaller, then rewards the AI 
+            ' 与前一个状态相比距离变小了，则奖励
+            controls = output.Select(Function(n) If(n.Value > 0.5, 1.0, 0.0)).ToArray
+        Else
+            ' Else if larger the distance, penalty
+            ' just press a random bottom on the game pad?
+            controls = New Double(4 - 1) {}
+            controls(randf.NextInteger(4)) = 1
+        End If
+
+        core.BackPropagate(controls, parallel:=False)
+
+        If Not game.Running Then
+            Call gameOver()
+        End If
+    End Sub
+
+    Private Sub gameOver()
+        ' if game over then restart the game
+        Call game.GameReset()
+    End Sub
 End Class
