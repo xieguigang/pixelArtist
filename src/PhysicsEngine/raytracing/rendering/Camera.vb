ed9b8f159f59b5491963721e3799ffc53eda65e1
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-20 14:15:28 +0800

    merge object model

--- src/PhysicsEngine/raytracing/rendering/Camera.vb
+++ src/PhysicsEngine/raytracing/rendering/Camera.vb
@@ -3,60 +3,29 @@
 Namespace raytracing.rendering
 
     Public Class Camera
-        Private positionField As Vector3
-        Private yawField As Single
-        Private pitchField As Single
-        Private fieldOfVision As Single
 
         Public Sub New()
-            positionField = New Vector3(0, 0, 0)
-            yawField = 0
-            pitchField = 0
-            fieldOfVision = 60
+            Position = New Vector3(0, 0, 0)
+            Yaw = 0
+            Pitch = 0
+            FOV = 60
         End Sub
 
         Public Overridable Property Position As Vector3
-            Get
-                Return positionField
-            End Get
-            Set(value As Vector3)
-                positionField = value
-            End Set
-        End Property
-
 
         Public Overridable Property Yaw As Single
-            Get
-                Return yawField
-            End Get
-            Set(value As Single)
-                yawField = value
-            End Set
-        End Property
-
 
         Public Overridable Property Pitch As Single
-            Get
-                Return pitchField
-            End Get
-            Set(value As Single)
-                pitchField = value
-            End Set
-        End Property
-
 
         Public Overridable Sub translate(vec As Vector3)
-            positionField.translate(vec)
+            Position = Position.translate(vec)
         End Sub
 
+        ''' <summary>
+        ''' field Of Vision
+        ''' </summary>
+        ''' <returns></returns>
         Public Overridable Property FOV As Single
-            Set(value As Single)
-                fieldOfVision = value
-            End Set
-            Get
-                Return fieldOfVision
-            End Get
-        End Property
 
     End Class
 
